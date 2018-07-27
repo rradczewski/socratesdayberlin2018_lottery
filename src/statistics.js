1d6352d9153efe0b20ba4968231c17d291292021
Raimo Radczewski <raimoradczewski@googlemail.com>
2018-07-27 13:39:55 +0200

    Simplify average calculation

--- src/statistics.js
+++ src/statistics.js
@@ -12,17 +12,16 @@ module.exports = (fn, n = 10000) => {
   for (let i = 0; i < n; i++) {
     const drawnElems = fn(random);
     for (let drawnElem of drawnElems) {
-      const group = `${groupForApplicant(drawnElem)}_${drawnElem.id}`
+      const group = `${groupForApplicant(drawnElem)}_${drawnElem.id}`;
       found[group] = (found[group] || 0) + 1;
     }
   }
 
   const p_values = R.map(v => v / n, found);
-
   const groups = R.groupBy(R.compose(R.head, R.split("_")), Object.keys(found));
-  const groupsCnt = R.map(R.compose(R.length, R.values), groups);
+
   const groupsSum = R.mapObjIndexed(
-    (members, group) => R.sum(R.props(members, p_values)) / groupsCnt[group],
+    (members, group) => R.sum(R.props(members, p_values)) / members.length,
     groups
   );
 
